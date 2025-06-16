20d643556e1467fbe2a916e0d4e358c3c4efd322
MeivanK20 <mouzonglandry@gmail.com>
2025-06-16 21:56:59 +0100

    Corrige next.config.js pour déploiement dynamique sur Vercel

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  reactStrictMode: true,
   images: {
-    unoptimized: true, // Désactive l’optimisation (obligatoire pour l’export statique)
-  },
-};
+    // Laisse cette option uniquement si tu veux désactiver l’optimisation d’image de Next.js.
+    // Tu peux la retirer complètement si tu veux activer l'optimisation par défaut de Vercel.
+    unoptimized: false 
+  }
+}
 
 module.exports = nextConfig;
